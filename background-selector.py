ba7731b1ed74dd8fda6233ae0aac6deebeb73634
Kizumi <jaychong1203@gmail.com>
2023-04-22 09:46:33 -0700

    broken arrow images, broken text , working slider

--- background-selector.py
+++ background-selector.py
@@ -18,34 +18,44 @@ left_selector = pygame.transform.scale(left_selector, [selector_width, selector_
 right_selector = pygame.transform.scale(right_selector, [selector_width, selector_height])
 
 images = ["360a8e769faff209fe61b202712f59a4-2540459211.png", 
-           "cat-10-e1573844975155-1818957124.png", 
-            "download.jpg"]
+          "cat-10-e1573844975155-1818957124.png", 
+          "download.jpg"]
+
+image_data = [("Cute Cat", (0, 0, 0)),   
+              ("Sleepy Cat", (255, 0, 0)),
+              ("Funny Cat", (0, 255, 0))]
+
 current_image_index = 0
-
-    
-
+current_text_index = 0
 
 current_image = pygame.image.load(images[current_image_index]).convert().convert_alpha()
 current_image = pygame.transform.scale(current_image, [500, 500])
+current_text_index = my_font.render('Cute Cat', False, (0, 0, 0))
 
 image_position = (screen_width // 2 - current_image.get_width() // 2, screen_height // 2 - current_image.get_height() // 2)
-text_surface = my_font.render('Cute Cat', False, (0, 0, 0))
+
+
 
 def display_current_image():
     screen.blit(current_image, image_position)
 
+
 def next_image():
-    global current_image_index, current_image
+    global current_image_index, current_image, current_text_index
     current_image_index = (current_image_index + 1) % len(images)
+    # current_text_index = (current_text_index + 1) % len(image_data)
     current_image = pygame.image.load(images[current_image_index]).convert().convert_alpha()
     current_image = pygame.transform.scale(current_image, [500, 500])
+    # current_text_index = my_font.render(image_data[current_text_index][0], False, image_data[current_text_index][1])
     display_current_image()
 
 def previous_image():
-    global current_image_index, current_image
+    global current_image_index, current_image, current_text_index
     current_image_index = (current_image_index - 1) % len(images)
+    # current_text_index = (current_text_index - 1) % len(image_data)
     current_image = pygame.image.load(images[current_image_index]).convert().convert_alpha()
     current_image = pygame.transform.scale(current_image, [500, 500])
+    # current_text_index = my_font.render(image_data[current_text_index][0], False, image_data[current_text_index][1])
     display_current_image()
 
 running = True
@@ -61,12 +71,11 @@ while running:
             elif event.key == pygame.K_ESCAPE:
                 running = False 
 
-
-        screen.fill((255, 255, 255))
+    screen.fill((255, 255, 255))
     display_current_image()
     screen.blit(left_selector, dest=(400, 540))
     screen.blit(right_selector, dest = (1400, 540))
-    screen.blit(text_surface, dest=(900, 800))
+    # screen.blit(current_text_index, dest=(900, 800))
     pygame.display.flip()
 
 pygame.quit()
