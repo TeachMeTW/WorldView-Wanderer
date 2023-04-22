0322d0dcabab74b95f21404707758c9242dbfec6
ShriyanGote <shriyan.gote@gmail.com>
2023-04-22 08:36:19 -0700

    first dropdown

---
+++ drop_down.py
@@ -0,0 +1,93 @@
+import pygame as pg
+import random
+
+class DropDown():
+
+    def __init__(self, color_menu, color_option, x, y, w, h, font, main, options):
+        self.color_menu = color_menu
+        self.color_option = color_option
+        self.rect = pg.Rect(x, y, w, h)
+        self.font = font
+        self.main = main
+        self.options = options
+        self.draw_menu = False
+        self.menu_active = False
+        self.active_option = -1
+        self.display_options = []
+
+    def draw(self, surf):
+        pg.draw.rect(surf, self.color_menu[self.menu_active], self.rect, 0)
+        msg = self.font.render(self.main, 1, (0, 0, 0))
+        surf.blit(msg, msg.get_rect(center = self.rect.center))
+
+        if self.draw_menu:
+            for i, text in enumerate(self.display_options):
+                rect = self.rect.copy()
+                rect.y += (i+1) * self.rect.height
+                pg.draw.rect(surf, self.color_option[1 if i == self.active_option else 0], rect, 0)
+                msg = self.font.render(text, 1, (0, 0, 0))
+                surf.blit(msg, msg.get_rect(center = rect.center))
+
+    def update(self, event_list):
+        mpos = pg.mouse.get_pos()
+        self.menu_active = self.rect.collidepoint(mpos)
+        
+        self.active_option = -1
+        for i in range(len(self.display_options)):
+            rect = self.rect.copy()
+            rect.y += (i+1) * self.rect.height
+            if rect.collidepoint(mpos):
+                self.active_option = i
+                break
+
+        if not self.menu_active and self.active_option == -1:
+            self.draw_menu = False
+
+        for event in event_list:
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                if self.menu_active:
+                    self.display_options = random.sample(self.options, 3)
+                    self.draw_menu = True
+                elif self.draw_menu and self.active_option >= 0:
+                    self.draw_menu = False
+                    return self.display_options[self.active_option]
+        return '-1'
+
+pg.init()
+clock = pg.time.Clock()
+screen = pg.display.set_mode((1080, 860))
+
+
+
+COLOR_ACTIVE = (148, 208, 242)
+COLOR_INACTIVE = (16, 109, 163)
+COLOR_LIST_INACTIVE = (98, 156, 102)
+COLOR_LIST_ACTIVE = (37, 94, 150)
+
+list1 = DropDown(
+    [COLOR_INACTIVE, COLOR_ACTIVE],
+    [COLOR_LIST_INACTIVE, COLOR_LIST_ACTIVE],
+    150, 50, 200, 40, 
+    pg.font.Font("assets/font.ttf", 40),
+    "Select Country", ["Canada", "China", "France", "Germany", "India", "Italy", "Japan", "Korea", "Mexico", "USA"])
+
+run = True
+while run:
+    clock.tick(30)
+
+    event_list = pg.event.get()
+    for event in event_list:
+        if event.type == pg.QUIT:
+            run = False
+
+    selected_option = list1.update(event_list)
+    if selected_option != '-1':
+        print(selected_option)
+        list1.main = 'Select Country'
+
+    screen.fill((255, 255, 255))
+    list1.draw(screen)
+    pg.display.flip()
+    
+pg.quit()
+exit()
