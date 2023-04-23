071ac8ed956b1a3e1c2a07531d6e9d186401345b
ShriyanGote <111940972+ShriyanGote@users.noreply.github.com>
2023-04-23 07:29:52 -0700

    Add files via upload

---
+++ countryimages/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  testfortransfer
+//
+//  Created by Shriyan Gote on 4/12/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello World!\n");
+    return 0;
+}
